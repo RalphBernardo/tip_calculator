bdba23976ede5be0081b9e5b852968d8e72f018e
Ralph Bernardo <ralphbernardo@gmail.com>
2013-10-12 11:46:58 -0400

    Version 3

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,6 +1,8 @@
-meal = float(raw_input("Enter the cost of your meal before tax: "))
-tax = float(raw_input("Enter the tax rate as a percentage: "))/100
-tip = float(raw_input("Enter your tip as a percentage: "))/100
+import sys
+
+meal = float(sys.argv[1])
+tax = float(sys.argv[2])/100
+tip = float(sys.argv[3])/100
 
 tax_value = meal * tax
 meal_with_tax = meal + tax_value
@@ -11,6 +13,6 @@ print "Your meal cost $%.2f" % meal + "."
 
 print "The tax on your meal is $%.2f" % tax_value + "."
 
-print "You tipped $%.2f" % tip_value + " on your meal."
+print "Tipping at a rate of {0}%, you tipped ${1:.2f} on your meal.".format(int(100*tip), tip_value)
 
 print "Your meal's total cost is $%.2f" % total + "."
