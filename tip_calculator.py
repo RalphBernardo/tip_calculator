9adf564a9368327c039bd18193a5343b1a430a07
Ralph Bernardo <ralphbernardo@gmail.com>
2013-10-15 13:03:42 -0400

    Version 4

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,8 +1,19 @@
-import sys
+from optparse import OptionParser
 
-meal = float(sys.argv[1])
-tax = float(sys.argv[2])/100
-tip = float(sys.argv[3])/100
+parser = OptionParser()
+parser.add_option("-m", "--meal", dest="meal", type="float", help="enter the cost of meal without tax")
+parser.add_option("-x", "--tax", dest="tax", type="float", help="enter the tax rate")
+parser.add_option("-t", "--tip", dest="tip", type="float", help="enter the tip percentage", default=15.0)
+
+(options, args) = parser.parse_args()
+if not options.meal:
+    parser.error("You must enter your meal cost without tax.")
+if not options.tax:
+    parser.error("You must enter the tax rate.")
+
+meal = options.meal
+tax = options.tax/100
+tip = options.tip/100
 
 tax_value = meal * tax
 meal_with_tax = meal + tax_value
